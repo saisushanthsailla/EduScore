9018d152988b5d37546df6f814d59bd9ea766aa2
Sai Sushanth Sailla <saisushanth777@gmail.com>
2026-02-10 23:44:09 +0530

    error fixing

--- src/components/data_injestion.py
+++ src/components/data_injestion.py
@@ -1,21 +1,17 @@
 import os
 import sys
-from src.exceptions import CustomException
+from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
-
-from src.components.model_train import ModelTrainerConfig
-from src.components.model_train import ModelTrainer
-
-
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -29,7 +25,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('notebooks/data/stud.csv')
+            df=pd.read_csv('notebook\data\stud.csv')
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -55,12 +51,13 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    train_data,test_data = obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
 
-    data_transformation = DataTransformation()
-    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
     modeltrainer=ModelTrainer()
     print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
 
 
--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -14,29 +14,29 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
 
-from src.exceptions import CustomException
+from src.exception import CustomException
 from src.logger import logging
 
 from src.utils import save_object,evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path = os.path.join("artifacts","model.pkl")
+    trained_model_file_path=os.path.join("artifacts","model.pkl")
 
-class ModelTrainer :
-    def __init__(self) :
-        self.model_trainer_config = ModelTrainerConfig()
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
 
-    def initiate_model_trainer(self,train_array,test_array) :
-        try :
-            logging.info("data splitting has started")
-            X_train,y_train,X_test,y_test = (
+
+    def initiate_model_trainer(self,train_array,test_array):
+        try:
+            logging.info("Split training and test input data")
+            X_train,y_train,X_test,y_test=(
                 train_array[:,:-1],
                 train_array[:,-1],
                 test_array[:,:-1],
                 test_array[:,-1]
             )
-
             models = {
                 "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
@@ -46,8 +46,6 @@ class ModelTrainer :
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
-
-
             params={
                 "Decision Tree": {
                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
@@ -86,16 +84,17 @@ class ModelTrainer :
                 
             }
 
-
-            model_report : dict =evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models,param = params)
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                             models=models,param=params)
+            
+            ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
+
+            ## To get best model name from dict
 
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
-
-
             best_model = models[best_model_name]
 
             if best_model_score<0.6:
@@ -111,8 +110,10 @@ class ModelTrainer :
 
             r2_square = r2_score(y_test, predicted)
             return r2_square
+            
 
-        except Exception as e :
+
+
+            
+        except Exception as e:
             raise CustomException(e,sys)
-
-
